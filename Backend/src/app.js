82b6faea857b0efcf880003de10b02a85c17e52b
Hilario <gabriel0hilario@gmail.com>
2021-09-21 09:52:50 -0300

    Rodando migration automaticamente

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,14 @@
 import "./database";
 import express from "express";
 import routes from "./routes";
-import cors from 'cors';
+import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 const swaggerFile = require(`../swagger_output.json`);
+import { exec } from "child_process";
 
 class App {
   constructor() {
+    this.database();
     this.server = express();
     this.middleware();
     this.routes();
@@ -20,6 +22,19 @@ class App {
   routes() {
     this.server.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
     this.server.use("/", routes);
+  }
+
+  database() {
+    exec("yarn migrate", (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+    });
   }
 }
 
