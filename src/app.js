1a260d93cb116a4ba525c3693abd2105c492a85b
Hilario <gabriel0hilario@gmail.com>
2021-09-20 22:54:52 -0300

    Adicionando cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import "./database";
 import express from "express";
 import routes from "./routes";
+import cors from 'cors';
 import swaggerUi from "swagger-ui-express";
 import swaggerFile from "/home/gabriel/projetos/digital-wallet/swagger_output.json";
 
@@ -12,6 +13,7 @@ class App {
   }
 
   middleware() {
+    this.server.use(cors());
     this.server.use(express.json());
   }
 
